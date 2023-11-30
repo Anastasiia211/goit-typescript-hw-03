69032f17e1378c207f847dd25f7d2ea75b4c2797
Anastasiia211 <nastik121@ukr.net>
2023-11-30 16:48:04 +0200

    hm 3 finish

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+    private signature: number = Math.random();
+
+    getSignature():number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+
+}
+
+abstract class House {
+    protected tenants: Person[] = []
+     protected door: boolean = false;
+    
+    constructor(protected key: Key) {}
+    
+    comeIn(person: Person):void {
+        if (this.door) {
+            this.tenants.push(person)
+            console.log(`Welcome home, ${person.getKey().getSignature()}!`)
+        } else {
+            console.log("The door is closed.");
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+} 
+
+class MyHouse extends House{
+    openDoor(key:Key): void{
+        if(this.key.getSignature()===key.getSignature()){
+            this.door=true;
+        }
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
